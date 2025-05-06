b0cf932e5c9c95705eccedae4eb97595d27c4dc7
Arnab Mukherjee <mukherjeearnab988@gmail.com>
2025-05-06 14:59:35 +0530

    Update UserForgetPassword.jsx

--- src/pages/UserForgetPassword.jsx
+++ src/pages/UserForgetPassword.jsx
@@ -150,13 +150,6 @@
 //                     className="block text-lg font-medium text-gray-700"
 //                   >
 //                     Password
-//                   </label>
-//                   <input
-//                     type={showPassword ? "text" : "password"}
-//                     className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
-//                     id="password"
-//                     name="password"
-//                     value={newPassword}
 //                     onChange={(e) => setNewPassword(e.target.value)}
 
 
