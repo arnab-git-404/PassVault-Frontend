5b616fd2ea9aae9bd008380b42b09369e3721660
Arnab Mukherjee <mukherjeearnab988@gmail.com>
2025-05-06 14:58:31 +0530

    Update UserForgetPassword.jsx

--- src/pages/UserForgetPassword.jsx
+++ src/pages/UserForgetPassword.jsx
@@ -216,16 +216,6 @@
 //                     Email
 //                   </label>
 //                   <input
-//                     type="email"
-//                     className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
-//                     id="email"
-//                     name="email"
-//                     value={email}
-//                     onChange={(e) => setEmail(e.target.value)}
-//                     placeholder="example@gmail.com"
-//                   />
-//                 </div>
-
 
 
 
