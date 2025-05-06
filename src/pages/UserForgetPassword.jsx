2cf29ae4f7bd4b6e31b2dffe99c40b2d91b546c9
Arnab Mukherjee <mukherjeearnab988@gmail.com>
2025-05-06 14:58:04 +0530

    Update UserForgetPassword.jsx

--- src/pages/UserForgetPassword.jsx
+++ src/pages/UserForgetPassword.jsx
@@ -254,18 +254,6 @@
 //                   </button>
 //                 </div>
 
-//                 <div className="flex justify-center">
-//                   <button
-//                     className=" hover:cursor-pointer w-full py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-//                     id="loginButton"
-//                     type="submit"
-//                   >
-//                     Submit OTP
-//                   </button>
-//                 </div>
-//               </form>
-//             )}
-//             <div>
 
 
 
