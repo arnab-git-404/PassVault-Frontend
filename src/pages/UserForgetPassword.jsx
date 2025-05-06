a0e304a74a48625d4dbb5aebb503c0fc3f441beb
Arnab Mukherjee <mukherjeearnab988@gmail.com>
2025-05-06 14:59:17 +0530

    Update UserForgetPassword.jsx

--- src/pages/UserForgetPassword.jsx
+++ src/pages/UserForgetPassword.jsx
@@ -170,15 +170,6 @@
 
 //                 <div className="mb-3 relative">
 //                   <label
-//                     htmlFor="cnfPassword"
-//                     className="block text-lg font-medium text-gray-700"
-//                   </label>
-//                   <input
-//                     type={showPassword ? "text" : "password"}
-//                     className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
-//                     id="cnfPassword"
-//                     name="cnfPassword"
-
 
 
 
